c63701105bc5de4e0cc6895b427b2242a6d2292d
milesmaroney <miles@milesmaroney.com>
2022-01-11 22:38:26 -0800

    WIP using raw style query, idk

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -21,12 +21,30 @@ const products = {
     });
   },
   fetchStyles: (req, res) => {
-    db.retrieveStyles(req.params.product_id).then((data) => {
-      let result = {
-        product_id: req.params.product_id,
-        results: data.rows,
-      };
-      res.status(200).send(result);
+    db.retrieveStylesPhotos(req.params.product_id).then((data) => {
+      for (let j = 0; j < data.rows.length; j++) {
+        data.rows[j].photos = [];
+        for (let i = j; i < data.rows.length; i++) {
+          if (
+            data.rows[i].style_id === data.rows[j].style_id &&
+            data.rows[j].size === data.rows[i].size
+          ) {
+            data.rows[j].photos.push({
+              thumbnail_url: data.rows[i].thumbnail_url,
+              url: data.rows[i].url,
+            });
+            delete data.rows[i].url;
+            delete data.rows[i].thumbnail_url;
+          } else {
+            break;
+          }
+        }
+      }
+      // let result = {
+      //   product_id: req.params.product_id,
+      //   results: data.rows,
+      // };
+      res.status(200).send(data.rows);
     });
   },
   fetchRelated: (req, res) => {},
