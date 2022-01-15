b0beb830ed3d4c73e69360ee0f568357022f0486
milesmaroney <miles@milesmaroney.com>
2022-01-15 10:50:32 -0800

    Removed morgan logging

--- server/index.js
+++ server/index.js
@@ -4,11 +4,9 @@ const app = express();
 const router = require('./routers');
 const path = require('path');
 
-const morgan = require('morgan');
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
 app.use(express.json());
 app.use(cors());
 
