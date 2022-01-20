cb92049b66a40bd35ab3433db4a3ea4a6eb80fb3
milesmaroney <miles@milesmaroney.com>
2022-01-20 12:03:48 -0800

    Added redis cache

--- server/index.js
+++ server/index.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const router = require('./routers');
 const path = require('path');
+const ExpressRedisCache = require('express-redis-cache');
+const cache = ExpressRedisCache({
+  expire: 120, // optional: expire every 10 seconds
+});
 
 const cors = require('cors');
 const PORT = process.env.PORT || 3000;
@@ -11,7 +15,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use('/api', router);
+app.use('/api', cache.route(), router);
 
 app.listen(PORT, () => {
   console.log(`Server started on ${PORT}`);
